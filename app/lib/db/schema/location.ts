3e49c60fcf61d7ea79e75f0b3a1bf9a6055dc073
doemsche <doemsche@gmx.ch>
2025-11-17 17:47:37 +0100

    add auth (#53)

--- app/lib/db/schema/location.ts
+++ app/lib/db/schema/location.ts
@@ -1,10 +1,13 @@
 import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
+
 export const location = sqliteTable("location", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
   slug: text().notNull().unique(),
   description: text(),
+  userId: int().notNull().references(() => user.id),
   lat: real().notNull(),
   long: real().notNull(),
   createdAt: int().notNull().$default(() => Date.now()),
